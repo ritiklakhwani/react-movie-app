486e93beb4dc0136c38dcb10bb5ef325f1b0da7d
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-03-19 12:36:52 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+const card = ({title}) => {
+
+}
+
+
+const App = () => {
+  return(
+    <h2>Functional Arrow Component</h2>
+  )
+}
+
+
+export default App
+
