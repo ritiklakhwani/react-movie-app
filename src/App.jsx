f5695707fded2a358cc9df41417c8c1e7a21e49a
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-03-20 07:19:40 +0530

    feat: added counters to show similar movies based on number of clicks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0)
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  })
+
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title}<br/> {count}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? "Liked" : "like"}</button>
     </div>
   );
 };
